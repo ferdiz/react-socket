cc2bc80567416f0ab60a2563b9cfb1feffd422cd
fedeziraldo <federrico04@gmail.com>
2018-12-29 16:40:31 -0300

    cuadrado

--- react-ejemplo/src/App.js
+++ react-ejemplo/src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-    this.setState({socket})
-    socket.on("chat", data => this.setState({ response: data}));
+    this.setState({ socket })
+    socket.on("chat", data => this.setState({ response: data }));
   }
   render() {
     const { response } = this.state;
@@ -22,12 +22,21 @@ class App extends Component {
       <div style={{ textAlign: "center" }}>
         <p> Ultimo mensaje: {response}</p>
         <input id="chat" />
-        <button onClick={this.chatear}>Enviar</button>
+        <button onClick={this.chatear}>Enviar</button><br></br>
+        <svg height="100" width="100">
+          <defs>
+            <clipPath id="circleView">
+              <polygon points="7,25 50,0 93,25 93,75 50,100 7,75" style={{ fill: "lime", stroke: "purple", strokeWidth: 1 }} />
+            </clipPath>
+          </defs>
+
+          <image width="100" height="100" href="pasto.jpeg" clipPath="url(#circleView)" />
+        </svg>
       </div>
     );
   }
 
-  chatear(){
+  chatear() {
     this.state.socket.emit("chat", document.getElementById("chat").value);
   }
 }
